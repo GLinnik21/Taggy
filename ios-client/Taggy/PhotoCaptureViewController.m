07235c7b842a97dc74353d683129720a3841a1fd
Nikolay Volosatov <bamx23@yandex-team.ru>
2014-10-27 15:49:48 +0300

    ios-tesseract: Replace sending image with tesseract recognition

--- ios-client/Taggy/PhotoCaptureViewController.m
+++ ios-client/Taggy/PhotoCaptureViewController.m
@@ -10,11 +10,11 @@
 #import "ViewController.h"
 #import "Data.h"
 #import "imageCell.h"
-#import <CoreData/CoreData.h>
+#import <TesseractOCR/TesseractOCR.h>
 
 static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
 
-@interface PhotoCaptureViewController() <NSURLConnectionDelegate>
+@interface PhotoCaptureViewController()
 
 @property (nonatomic, weak) IBOutlet UIImageView *imageview;
 
@@ -38,7 +38,9 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     image = [info objectForKey:UIImagePickerControllerOriginalImage];
-    [self sendImage:image];
+
+    [self recognizeImage:image];
+
     Data *item = [[Data alloc] init];
     item.image = image;
     item.Btransf = @"$";
@@ -53,62 +55,17 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
-- (void)sendImage:(UIImage *)sendingImage
+- (void)recognizeImage:(UIImage *)imageToRecognize
 {
-    //Activate the status bar spinner
-    UIApplication* app = [UIApplication sharedApplication];
-    app.networkActivityIndicatorVisible = YES;
-    
-    //The image you want to upload represented in JPEG
-    //NOTE: the 'selectedPhoto' needs to be replaced with the UIImage you'd like to upload
-    NSData *imageData = UIImageJPEGRepresentation(sendingImage, 1);
-    
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:kSendingURL]];
-    [request setHTTPMethod:@"POST"];
-    
-    NSMutableData *body = [NSMutableData data];
-    
-    NSString *boundary = @"---------------------------14737809831466499882746641449";
-    NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", boundary];
-    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
-    
-    //The file to upload
-    [body appendData:[[NSString stringWithFormat:@"--%@\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[[NSString stringWithFormat:@"Content-Disposition: attachment; name=\"file\"; filename=\"image.jpg\"\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[NSData dataWithData:imageData]];
-    [body appendData:[@"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    
-    // close the form
-    [body appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    // set request body
-    [request setHTTPBody:body];
-    
-    // Configure your request here.  Set timeout values, HTTP Verb, etc.
-    NSURLConnection *connection = [NSURLConnection connectionWithRequest:request delegate:self];
-    
-    //Stop the status bar spinner
-    app.networkActivityIndicatorVisible = NO;
-}
+    Tesseract* tesseract = [[Tesseract alloc] initWithLanguage:@"rus"];
+    [tesseract setImage:imageToRecognize];
+    [tesseract recognize];
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response
-{
-    // This method is called when the server has determined that it
-    // has enough information to create the NSURLResponse object.
-    
-    // It can be called multiple times, for example in the case of a
-    // redirect, so each time we reset the data.
-    
-    // receivedData is an instance variable declared elsewhere.
-    [[[UIAlertView alloc] initWithTitle:@"Отправлено!"
-                               message:response.description
-                              delegate:nil
-                     cancelButtonTitle:@"ОК"
-                     otherButtonTitles:nil]show];
-    
+    [[[UIAlertView alloc] initWithTitle:@"Распознанный текст"
+                                message:[tesseract recognizedText]
+                               delegate:nil
+                      cancelButtonTitle:@"ОК"
+                      otherButtonTitles:nil]show];
 }
 
 @end
