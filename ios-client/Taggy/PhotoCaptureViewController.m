751b939c1c10f6e4f52572db541660aeb19672df
Nikolay Volosatov <bamx23@yandex-team.ru>
2014-10-28 11:11:39 +0300

    ios-tesseract: Add near blocks joining. First returning values

--- ios-client/Taggy/PhotoCaptureViewController.m
+++ ios-client/Taggy/PhotoCaptureViewController.m
@@ -39,14 +39,14 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
 
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     image = [info objectForKey:UIImagePickerControllerOriginalImage];
-    image = [UIImage imageNamed:@"3.png"];
 
-    [self recognizeImage:image];
+    NSNumber *recognizedValue = [self recognizeImage:image];
+    NSNumber *converted = @([recognizedValue floatValue] / 35);
 
     Data *item = [[Data alloc] init];
     item.image = image;
-    item.Btransf = @"$";
-    item.Atransf = @"%";
+    item.Btransf = [NSString stringWithFormat:@"%@ $", converted];
+    item.Atransf = [NSString stringWithFormat:@"%@ руб", recognizedValue];
     [Data addObject:item];
     //[self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:[NSIndexPath indexPathForRow:self.dataAr.count - 1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
     [self.imageview setImage:image];
@@ -57,7 +57,7 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
-- (void)recognizeImage:(UIImage *)imageToRecognize
+- (NSNumber *)recognizeImage:(UIImage *)imageToRecognize
 {
     Tesseract* tesseract = [[Tesseract alloc] initWithLanguage:@"rus"];
     tesseract.delegate = self;
@@ -76,6 +76,8 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
     }];
 
     NSMutableArray *goodWords = [[NSMutableArray alloc] init];
+    NSMutableSet *badWords = [[NSMutableSet alloc] init];
+
     for (NSDictionary *dict in conf) {
         NSNumber *confidence = dict[@"confidence"];
         NSString *data = dict[@"text"];
@@ -83,19 +85,71 @@ static NSString *const kSendingURL = @"http://taggy-api.bx23.net/Home/Convert";
 
         data = [data stringByReplacingOccurrencesOfString:@"," withString:@"."];
         CGFloat value = [data floatValue];
-        if (value == 0) continue;
+        if (value == 0) {
+            if (data.length < 2) {
+                [badWords addObject:dict];
+            }
+            continue;
+        }
         if (ABS(rect.size.height) < 30) continue;
         //if (((int)value & 10) != 0 || value < 50) continue;
 
         [goodWords addObject:dict];
     }
 
+    NSMutableSet *results = [[NSMutableSet alloc] init];
+
+    bool anyFound = false;
+    do {
+        NSMutableArray *newGoodWords = [[NSMutableArray alloc] initWithArray:goodWords];
+        for (NSDictionary *dict in conf) {
+            if ([badWords containsObject:dict]) continue;
+
+            NSNumber *confidence = dict[@"confidence"];
+            NSString *data = dict[@"text"];
+            CGRect rect = [((NSValue *)dict[@"boundingbox"]) CGRectValue];
+
+            for (NSDictionary *exDict in goodWords) {
+                NSString *exData = exDict[@"text"];
+                CGRect exRect = [((NSValue *)exDict[@"boundingbox"]) CGRectValue];
+
+                CGFloat topDelta = ABS(CGRectGetMinY(rect) - CGRectGetMinY(exRect));
+                CGFloat bottomDelta = ABS(CGRectGetMaxY(rect) - CGRectGetMaxY(exRect));
+                CGFloat leftDistDelta = ABS(CGRectGetMinX(rect) - CGRectGetMaxX(exRect));
+                CGFloat rightDistDelta = ABS(CGRectGetMaxX(rect) - CGRectGetMinX(exRect));
+
+                NSString *unionedResult = nil;
+                if (topDelta < 10 && bottomDelta < 10) {
+                    if (leftDistDelta > 10 || rightDistDelta > 10) {
+                        if (leftDistDelta < 15) {
+                            NSLog(@"new word: %@ + %@", exData, data);
+                            unionedResult = [exData stringByAppendingString:data];
+                        }
+                        if (rightDistDelta < 15) {
+                            NSLog(@"new word: %@ + %@", data, exData);
+                            unionedResult = [data stringByAppendingString:exData];
+                        }
+                    }
+                }
+
+                if (unionedResult != nil) {
+                    CGFloat value = [[unionedResult stringByReplacingOccurrencesOfString:@"," withString:@"."] floatValue];
+                    [results addObject:@(value)];
+                    if ([goodWords containsObject:dict] == NO) {
+                        [newGoodWords addObject:dict];
+                    }
+                }
+            }
+        }
+        goodWords = newGoodWords;
+    } while (anyFound);
 
     [[[UIAlertView alloc] initWithTitle:@"Распознанный текст"
-                                message:[goodWords description]
+                                message:[[[results objectEnumerator] allObjects] description]
                                delegate:nil
                       cancelButtonTitle:@"ОК"
                       otherButtonTitles:nil]show];
+    return [[results allObjects] firstObject];
 }
 
 @end
