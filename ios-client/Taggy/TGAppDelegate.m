1e4b4f3f4ca42a60d97ad34df5909f6e9c75da3d
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-01-10 17:51:09 +0300

    Add Debug/Release differ

--- ios-client/Taggy/TGAppDelegate.m
+++ ios-client/Taggy/TGAppDelegate.m
@@ -19,16 +19,28 @@
 @implementation TGAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+#ifdef DEBUG
     [ARAnalytics setupWithAnalytics:@{
                                       ARFlurryAPIKey : @"R28M5M82FH2X33XCQW4N",
                                       ARGoogleAnalyticsID : @"UA-9189602-6",
                                       ARYandexMobileMetricaAPIKey : @"30144",
                                       }];
+#else
+    [ARAnalytics setupWithAnalytics:@{
+                                      ARFlurryAPIKey : @"6QPH2WFQCQRJHKPNHYTS",
+                                      ARGoogleAnalyticsID : @"UA-9189602-7",
+                                      ARYandexMobileMetricaAPIKey : @"31233",
+                                      }];
+#endif
 
     [TGMigrationManager migrate];
     
     [TGCurrencyManager updateWithCallback:nil];
+
+#ifdef DEBUG
     [TGDataManager fillSample];
+#endif
 
     return YES;
 }
