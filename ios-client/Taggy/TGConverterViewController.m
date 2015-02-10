857518ee6ea09fd50a489161e0082aeae72c70b8
Gleb Linkin <gleb_linnik@icloud.com>
2015-02-10 19:31:58 +0300

    2-nd picker

--- ios-client/Taggy/TGConverterViewController.m
+++ ios-client/Taggy/TGConverterViewController.m
@@ -13,7 +13,9 @@
 
 @property (strong, nonatomic) NSArray *dataSource;
 @property (nonatomic, retain) UIPickerView *sellPickerView;
-@property (nonatomic, assign) BOOL *checkSell;
+@property (nonatomic, assign) bool *checkSell;
+@property (nonatomic, retain) UIPickerView *buyPickerView;
+@property (nonatomic, assign) bool *checkBuy;
 
 @end
 
@@ -36,6 +38,7 @@
     
     [toolBar setItems:@[flexSpace, barButtonDone] animated:YES];
     self.sellTextField.inputAccessoryView = toolBar;
+    self.buyTextField.inputAccessoryView = toolBar;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -69,36 +72,74 @@
             make.top.equalTo(self.sellPickerView);
             
         }];
-        self.checkSell = true;
+        self.checkSell = false;
+        self.checkBuy = false;
+        [self.buyPickerView removeFromSuperview];
     }
-    [self.sellTextField endEditing:YES];
+    [self.sellTextField resignFirstResponder];
+    [self.buyTextField resignFirstResponder];
 }
 
 - (IBAction)buyAction:(id)sender {
+    if (self.checkBuy == false) {
+        [self.buyButton setTitle:@"Changing..." forState:UIControlStateNormal];
+        self.buyPickerView = [[UIPickerView alloc] init];
+        [self.buyPickerView setDelegate:self];
+        self.buyPickerView.showsSelectionIndicator = YES;
+        UIToolbar *toolBar= [[UIToolbar alloc] init];
+        UIBarButtonItem *flexSpace = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+        
+        UIBarButtonItem *barButtonDone = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(changeSellCurrency:)];
+        
+        [toolBar setItems:@[flexSpace, barButtonDone] animated:YES];
+        [toolBar sizeToFit];
+        
+        [self.view insertSubview:self.buyPickerView atIndex:0];
+        [self.buyPickerView insertSubview:toolBar atIndex:10];
+        [self.buyPickerView mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(self.view);
+            make.height.equalTo(self.view).multipliedBy(0.45);
+            make.bottom.equalTo(self.view);
+        }];
+        [toolBar mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.width.equalTo(self.buyPickerView);
+            make.top.equalTo(self.buyPickerView);
+            
+        }];
+        self.checkSell = false;
+        self.checkBuy = false;
+        [self.sellPickerView removeFromSuperview];
+    }
+    [self.sellTextField resignFirstResponder];
+    [self.buyTextField resignFirstResponder];
 }
 
-- (void)pickerView:(UIPickerView *)sellPickerView didSelectRow: (NSInteger)row inComponent:(NSInteger)component {
-    // Handle the selection
-    [self.sellButton setTitle:[self.dataSource objectAtIndex:row] forState:UIControlStateNormal];
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow: (NSInteger)row inComponent:(NSInteger)component {
+    if (pickerView = self.sellPickerView) {
+        [self.sellButton setTitle:[self.dataSource objectAtIndex:row] forState:UIControlStateNormal];
+    }else if (pickerView = self.buyPickerView){
+        [self.buyButton setTitle:[self.dataSource objectAtIndex:row] forState:UIControlStateNormal];
+    }
+    
 }
 
 // tell the picker how many rows are available for a given component
-- (NSInteger)pickerView:(UIPickerView *)sellPickerView numberOfRowsInComponent:(NSInteger)component {
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
     return self.dataSource.count;
 }
 
 // tell the picker how many components it will have
-- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)sellPickerView {
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
     return 1;
 }
 
 // tell the picker the title for a given component
-- (NSString *)pickerView:(UIPickerView *)sellPickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
     return [self.dataSource objectAtIndex:row];
 }
 
 // tell the picker the width of each row for a given component
-- (CGFloat)pickerView:(UIPickerView *)sellPickerView widthForComponent:(NSInteger)component {
+- (CGFloat)pickerView:(UIPickerView *)pickerView widthForComponent:(NSInteger)component {
     int sectionWidth = 300;
     
     return sectionWidth;
@@ -113,18 +154,24 @@
     self.sellPickerView.alpha = self.sellPickerView.alpha * (-1) + 1;
     [UIView commitAnimations];
     self.checkSell = false;
+    self.checkBuy = false;
     NSLog(@"Done");
 }
-- (IBAction)sellEdititngDidBegin:(id)sender {
+- (IBAction)sellEditingDidBegin:(id)sender {
     [self.sellPickerView removeFromSuperview];
+    [self.buyPickerView removeFromSuperview];
     self.checkSell = false;
-    [self.sellTextField setDelegate:self];
-    [self.sellTextField setReturnKeyType:UIReturnKeyDone];
-    [self.sellTextField addTarget:self action:@selector(sellTextFieldEndEditing:) forControlEvents:UIControlEventEditingDidEndOnExit];
+    self.checkBuy = false;
 }
-
+- (IBAction)buyEditingDidBegin:(id)sender {
+    [self.sellPickerView removeFromSuperview];
+    [self.buyPickerView removeFromSuperview];
+    self.checkSell = false;
+    self.checkBuy = false;
+}
 - (void)sellTextFieldEndEditing: (id)sender {
     [self.sellTextField resignFirstResponder];
+    [self.buyTextField resignFirstResponder];
 }
 
 @end
