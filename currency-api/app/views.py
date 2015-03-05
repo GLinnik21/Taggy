80f854f0d8bf3d633ffc2872d35485d342ff2389
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-03-05 14:09:11 +0300

    Convert tabs to spaces. Add empty lines at the end

--- currency-api/app/views.py
+++ currency-api/app/views.py
@@ -2,32 +2,32 @@ from flask import jsonify, abort, make_response, render_template
 from app import app, auth, db, models
 
 def latestRates():
-	rates = models.Rate.query.group_by('name').all()
-	return rates
+    rates = models.Rate.query.group_by('name').all()
+    return rates
 
 def ratesHistory(count):
-	rates = dict()
-	query = db.session.query(models.Rate.date).distinct().order_by(models.Rate.date.desc())
-	if count > 0:
-		query = query.limit(count)
+    rates = dict()
+    query = db.session.query(models.Rate.date).distinct().order_by(models.Rate.date.desc())
+    if count > 0:
+        query = query.limit(count)
 
-	dates = [date[0] for date in query.all()]
+    dates = [date[0] for date in query.all()]
 
-	for date in dates:
-		dateRates = models.Rate.query.filter(models.Rate.date == date).all()
-		for rate in dateRates:
-			if rate.name in rates:
-				rates[rate.name][str(rate.date)] = rate.value
-			else:
-				rates[rate.name] = {str(rate.date): rate.value}
+    for date in dates:
+        dateRates = models.Rate.query.filter(models.Rate.date == date).all()
+        for rate in dateRates:
+            if rate.name in rates:
+                rates[rate.name][str(rate.date)] = rate.value
+            else:
+                rates[rate.name] = {str(rate.date): rate.value}
 
-	return rates
+    return rates
 
 def ratesToDict(rates):
-	result = dict()
-	for rate in rates:
-		result[rate.name] = rate.value
-	return result
+    result = dict()
+    for rate in rates:
+        result[rate.name] = rate.value
+    return result
 
 @app.errorhandler(404)
 def not_found(error):
@@ -35,26 +35,26 @@ def not_found(error):
 
 @app.route('/')
 def index():
-	rates = latestRates()
-	return render_template('index.html', rates = rates)
+    rates = latestRates()
+    return render_template('index.html', rates = rates)
 
 @app.route('/rates', methods=['GET'])
 def getTasks():
-	rates = ratesToDict(latestRates());
-	return jsonify(rates)
+    rates = ratesToDict(latestRates());
+    return jsonify(rates)
 
 @app.route('/rates/<string:rateIds>', methods=['GET'])
 #@auth.login_required
 def getTask(rateIds):
-	rates = ratesToDict(latestRates())
-	resultRates = dict()
-	for rateId in rateIds.split('+'):
-		if not rateId in rates:
-			abort(404)
-		resultRates[rateId] = rates[rateId]
-	return jsonify(resultRates)
+    rates = ratesToDict(latestRates())
+    resultRates = dict()
+    for rateId in rateIds.split('+'):
+        if not rateId in rates:
+            abort(404)
+        resultRates[rateId] = rates[rateId]
+    return jsonify(resultRates)
 
 @app.route('/history/<int:count>', methods=['GET'])
 def getHistory(count):
-	rates = ratesHistory(count);
-	return jsonify(rates)
+    rates = ratesHistory(count);
+    return jsonify(rates)
