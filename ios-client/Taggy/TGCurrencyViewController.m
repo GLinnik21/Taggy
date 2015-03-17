d82e10635b510f8fb1102ac287e11be60d27da32
Nikolay Volosatov <bamx23@yandex-team.ru>
2015-03-17 16:51:13 +0300

    Fix codestyle and few small problems

--- ios-client/Taggy/TGCurrencyViewController.m
+++ ios-client/Taggy/TGCurrencyViewController.m
@@ -13,25 +13,28 @@
 
 @property (nonatomic, retain) NSIndexPath *checkedIndexPath;
 
+@property (nonatomic, strong) NSMutableArray *codes;
+@property (nonatomic, strong) NSMutableArray *rates;
+@property (nonatomic, copy) NSArray *searchResults;
+
 @end
 
 @implementation TGCurrencyViewController
 
-NSMutableArray *codes;
-NSMutableArray *rates;
-NSArray *searchResults;
-
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    codes = [NSMutableArray array];
+
+    self.codes = [NSMutableArray array];
     for (TGCurrency *currency in [TGCurrency allObjects]) {
-        [codes addObject:currency.code];
+        [self.codes addObject:currency.code];
     }
     
-    rates = [NSMutableArray array];
+    self.rates = [NSMutableArray array];
     for (TGCurrency *currency in [TGCurrency allObjects]) {
- //       [rates addObject:currency.value];
+        [self.rates addObject:@(currency.value)];
     }
+
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
@@ -42,127 +45,80 @@ NSArray *searchResults;
 - (void)filterContentForSearchText:(NSString*)searchText scope:(NSString*)scope
 {
     NSPredicate *resultPredicate = [NSPredicate
-                                    predicateWithFormat:@"?????? contains[cd] %@", //не ясно из чего брать
+                                    predicateWithFormat:@"SELF contains[cd] %@", //не ясно из чего брать
                                     searchText];
     
-    searchResults = [codes filteredArrayUsingPredicate:resultPredicate];
+    self.searchResults = [self.codes filteredArrayUsingPredicate:resultPredicate];
 }
 
--(BOOL)searchDisplayController:(UISearchDisplayController *)controller
-shouldReloadTableForSearchString:(NSString *)searchString
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller
+        shouldReloadTableForSearchString:(NSString *)searchString
 {
-    [self filterContentForSearchText:searchString
-                               scope:[[self.searchDisplayController.searchBar scopeButtonTitles]
-                                      objectAtIndex:[self.searchDisplayController.searchBar
-                                                     selectedScopeButtonIndex]]];
+    NSInteger scopeIndex = self.searchDisplayController.searchBar.selectedScopeButtonIndex;
+    NSString *scope = [self.searchDisplayController.searchBar.scopeButtonTitles objectAtIndex:scopeIndex];
+
+    [self filterContentForSearchText:searchString scope:scope];
     
     return YES;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    // Return the number of sections.
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
     if (tableView == self.searchDisplayController.searchResultsTableView) {
-        return [searchResults count];
-    }else{
-    return [codes count];
+        return self.searchResults.count;
+    }
+    else {
+        return self.codes.count;
     }
 }
 
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Тут приходит другая таблица. И вообще, такой метод поиска деприкейтед в iOS 8. Поищите как это правильно делать в 8й оси.
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"currencyCell" forIndexPath:indexPath];
    // NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
 
-    
-    if([self.checkedIndexPath isEqual:indexPath])
-    {
+    if([self.checkedIndexPath isEqual:indexPath]) {
         cell.accessoryType = UITableViewCellAccessoryCheckmark;
     }
-    else
-    {
+    else {
         cell.accessoryType = UITableViewCellAccessoryNone;
     }
     
     if (tableView == self.searchDisplayController.searchResultsTableView) {
-        cell.textLabel.text = [searchResults objectAtIndex:indexPath.row];
-    } else {
-        cell.textLabel.text = [codes objectAtIndex:indexPath.row];
+        cell.textLabel.text = self.searchResults[indexPath.row];
     }
-   // cell.detailTextLabel.text = [rates objectAtIndex:indexPath.row];;
+    else {
+        cell.textLabel.text = self.codes[indexPath.row];
+    }
+   // cell.detailTextLabel.text = self.rates[indexPath.row];
     
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    if(self.checkedIndexPath)
-    {
-        UITableViewCell* uncheckCell = [tableView
-                                        cellForRowAtIndexPath:self.checkedIndexPath];
+    if (self.checkedIndexPath != nil) {
+        UITableViewCell *uncheckCell = [tableView cellForRowAtIndexPath:self.checkedIndexPath];
         uncheckCell.accessoryType = UITableViewCellAccessoryNone;
     }
-    UITableViewCell* cell = [tableView cellForRowAtIndexPath:indexPath];
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
-    [defaults setObject:[codes objectAtIndex:indexPath.row] forKey:@"country"];
+
+    // Тут ключ должен быть в разных вьюхах разный.
+    [defaults setObject:[self.codes objectAtIndex:indexPath.row] forKey:@"country"];
     cell.accessoryType = UITableViewCellAccessoryCheckmark;
     self.checkedIndexPath = indexPath;
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
